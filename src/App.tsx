2c8d1ef68f720e3463f5eca51b55ec1741491a5b
[dyad] <kithucherry@gmail.com>
2026-07-20 14:48:22 +0200

    [dyad] Silenced React Router future flag warning - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,11 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_relativeSplatPath: true,
+        }}
+      >
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
